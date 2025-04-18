796f94e3275f7edc1a5368e0fe8500cb0107489c
heenasingh-2368 <131607193+heenasingh-2368@users.noreply.github.com>
2025-04-18 20:05:53 +0530

    Add files via upload

---
+++ ASS1_task2.py
@@ -0,0 +1,4 @@
+str1=input("Enter your First Name:")
+str2=input("Enter second Name:")
+str=str1+str2
+print("Hello",str,"Welcome to Python") 
---
+++ Ass1_task1.py
@@ -0,0 +1,21 @@
+"""Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+
+a=int(input("Enter First number:"))
+b=int(input("Enter second number:"))
+c=a+b
+print("The Addition is=",c)
+c=a-b
+print("The Subtraction is=",c)
+c=a*b
+print("The Multiplication is=",c)
+c=a/b
+print("The Division is=",c)
